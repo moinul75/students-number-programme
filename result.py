b8cdb958dd5eccc31e6112a99a332e6234aadfeb
moinul75 <102654562+moinul75@users.noreply.github.com>
2022-03-30 14:09:11 +0600

    Update result.py

--- result.py
+++ result.py
@@ -1,1 +1,15 @@
-
+marks = int(input("Enter Your  Number: "))
+if 80 <= marks <=100:
+  print("A+")
+elif 70 <= marks<= 79:
+  print("A")
+elif 60 <= marks <= 69:
+  print("A-")
+elif 50<= marks <=59:
+  print("B")
+elif 40<=marks <=49:
+  print("C")
+elif 33<=marks <=39:
+  print("D")
+else:
+  print("Fail")
